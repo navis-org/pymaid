51f53e9d9f51ad9a60b5090fbeaa52309394b2bd
Philipp Schlegel <observing@web.de>
2017-02-09 15:10:43 +0000

    Added license information to plotneuron

--- plotneuron.py
+++ plotneuron.py
@@ -1,3 +1,21 @@
+"""
+    This script is part of pymaid (http://www.github.com/schlegelp/pymaid).
+    Copyright (C) 2017 Philipp Schlegel
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along
+
+
 import matplotlib.pyplot as plt 
 import matplotlib.lines as mlines
 import random, colorsys
