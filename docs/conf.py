21e2f4999e2f8495b49e68e0d4de0c65ec96755a
Philipp Schlegel <observing@web.de>
2017-07-11 12:57:03 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,8 @@ import os
 import sys
 import numpydoc
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 #sys.path.insert(0, '/Users/philipps/OneDrive/Cloudbox/Python/PyMaid/pymaid')
 #sys.path.insert(0, '/Users/philipps/OneDrive/Cloudbox/Python/PyMaid')
 
