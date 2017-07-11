9671ae6b263c543385df42f2c9ef18fe4b747007
Philipp Schlegel <observing@web.de>
2017-07-11 18:41:12 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 import mock
 MOCK_MODULES = ['numpy', 'scipy', 'sklearn', 'matplotlib', 'matplotlib.pyplot', 'igraph' , 'tqdm', 'pandas',  
-                'pylab', 'rpy2', 'plotly', 'plotly.plotly', 'plotly.offline', 'plotly.graph_objs', 'vispy','seaborn','scipy.spatial', 'ConvexHull', 'scipy.spatial.ConvexHull', 
+                'pylab', 'rpy2', 'rpy2.robjects','rpy2.robjects.packages','rpy2.robjects.packages.importr', 'rpy2.robjects.pandas2ri' ,'plotly', 'plotly.plotly', 'plotly.offline', 'plotly.graph_objs', 'vispy','seaborn','scipy.spatial', 'ConvexHull', 'scipy.spatial.ConvexHull', 
                 'matplotlib.lines', 'maplotlib.patches', 'matplotlib.collections', 'vispy.geometry']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
