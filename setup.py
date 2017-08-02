baa37271b3185ca8d5792335a1e1d002a19c757a
Philipp Schlegel <observing@web.de>
2017-08-02 11:54:28 +0100

    switch to setuptools
    distutils only as fallback

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+    
 import re
 
 
@@ -17,19 +21,20 @@ setup(
     version=verstr,
     packages=['pymaid',],
     license='GNU GPL V3',
+    description='Python interface with CATMAID',
     long_description=open('README.md').read(),
     url = 'https://github.com/schlegelp/pymaid',
     author='Philipp Schlegel',
     author_email = 'pms70@cam.ac.uk',
-    keywords='CATMAID interface neurons',
+    keywords='CATMAID interface neuron blender3d',
 
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         
-        'Intended Audience :: CATMAID Users',
-        'Topic :: CATMAID :: Interface',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
 
-        'License :: GNU GPL V3',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
@@ -37,6 +42,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
+
+    setup_requires=[ 'numpy>=1.12.1' ], #pyoctree's install requires numpy to install but didn't specify that, so we have to do that for it
 
     install_requires=[
         "python-igraph>=0.7.1",
@@ -51,4 +58,6 @@ setup(
         "pyoctree>=0.2.2"
         #"rpy2>=2.8.5", #This throws an error when no R is installed on the system 
     ],
+
+    python_requires='>=3',
 )
