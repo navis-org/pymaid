37580e9852bdfb72eff2c67fc2c9619a2140cb39
Philipp Schlegel <schlegelp@users.noreply.github.com>
2018-03-12 00:11:02 +0000

    try fixing syntax highlighting (again)

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     #'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx.ext.mathjax'
     #'numpydoc'
 ]
 
@@ -112,7 +113,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
