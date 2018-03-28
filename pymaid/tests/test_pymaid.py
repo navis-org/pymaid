93be3a819886ac957f753980a3670f0289631b86
Philipp Schlegel <schlegelp@users.noreply.github.com>
2018-03-29 00:27:34 +0100

    fixed tests
    - fixed broken tests
    - prepared test suite for Travis CI

--- pymaid/tests/test_pymaid.py
+++ pymaid/tests/test_pymaid.py
@@ -1,9 +1,7 @@
-""" Test suite for pymaid.pymaid
+""" Test suite for pymaid
 
-This test suite requires that you have a config_test.py in the /pymaid/test 
-that contains your CATMAID credentials credentials. 
-
-config_test.py should look like this:
+This test suite requires a bunch of variables that need to either defined
+as environment variables or in a config_test.py file
 
     #Catmaid server url
     server_url = ''
@@ -16,6 +14,15 @@ config_test.py should look like this:
 
     #Auth token
     token = ''  
+
+    # Test skeleton IDs
+    test_skids = []
+
+    # Test annotations
+    test_annotations = []
+
+    # Test CATMAID volume
+    test_volume = ''
 
 
 Examples
@@ -33,29 +40,41 @@ From shell:
 """
 
 import unittest
+import os
 
-from pymaid import fetch, core, plot, user_stats, graph, cluster
+import pymaid
 import pandas as pd
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# Silence most loggers
-user_stats.module_logger.setLevel('ERROR')
-graph.module_logger.setLevel('ERROR')
-cluster.module_logger.setLevel('ERROR')
-plot.module_logger.setLevel('ERROR')
-fetch.module_logger.setLevel('ERROR')
+# Silence module loggers
+pymaid.set_loggers('ERROR')
 
-try:
-    import config_test
-except:
-    raise ImportError('Unable to import configuration file.')
+# Deactivate progress bars
+pymaid.set_pbars(hide=True)
 
-# These skeleton ids and annotations will only work on FAFB
-example_skids = ['16', '2333007']
-example_annotations = ['glomerulus DA1', 'MVP2', 'mAL']
-example_volumes = ['v13.LH_R', 'v13.AL_R']
+# Try getting credentials from environment variable
+required_variables = ['server_url', 'http_user', 'http_pw', 'token', 
+                      'test_skids', 'test_annotations', 'test_volume']
+
+if False not in [v in os.environ for v in required_variables]:
+    class conf: pass
+    config_test = conf()
+
+    for v in ['server_url', 'http_user', 'http_pw', 'token', 'test_volume']:
+        setattr(config_test, v, os.environ[v].split(',')) 
+
+    for v in ['test_skids', 'test_annotations']:
+        setattr(config_test, v, os.environ[v]) 
+else:
+    missing = [v for v in required_variables if v not in os.environ]
+    print('Missing some environment variables:', ','.join(missing))
+    print('Falling back to config_test.py')
+    try:
+        import config_test
+    except:
+        raise ImportError('Unable to import configuration file.')
 
 
 class TestPymaid(unittest.TestCase):
@@ -74,18 +93,20 @@ class TestPymaid(unittest.TestCase):
         self.assertIn('annotations', self.rm.fetch(remote_annotation_list_url))
 
     def test_eval_skids(self):
+        """ Test skeleton ID evaluation. """
         self.assertEqual(pymaid.eval_skids(
             ['12345', '67890'], remote_instance=self.rm), ['12345', '67890'])
         self.assertEqual(pymaid.eval_skids(
             [12345, 67890], remote_instance=self.rm), ['12345', '67890'])
         self.assertIsNotNone(pymaid.eval_skids(
-            example_annotations, remote_instance=self.rm))
+            'annotation:{}'.format(config_test.test_annotations[0]), 
+            remote_instance=self.rm))
 
     def test_neuron_exists(self):
         self.assertIsInstance(pymaid.neuron_exists(
-            example_skids[0], remote_instance=self.rm), bool)
+            config_test.test_skids[0], remote_instance=self.rm), bool)
         self.assertIsInstance(pymaid.neuron_exists(
-            example_skids, remote_instance=self.rm), dict)
+            config_test.test_skids, remote_instance=self.rm), dict)
 
     def test_get_neuron_list(self):
         self.assertIsInstance(pymaid.get_neuron_list(
@@ -101,33 +122,33 @@ class TestPymaid(unittest.TestCase):
 
     def test_get_annotated_skids(self):
         self.assertIsInstance(pymaid.get_skids_by_annotation(
-            example_annotations[0], remote_instance=self.rm), list)
+            config_test.test_annotations[0], remote_instance=self.rm), list)
 
     def test_get_annotations(self):
         self.assertIsInstance(pymaid.get_annotations(
-            example_skids, remote_instance=self.rm), dict)
+            config_test.test_skids, remote_instance=self.rm), dict)
         self.assertIsInstance(pymaid.get_annotation_details(
-            example_skids, remote_instance=self.rm), pd.DataFrame)
+            config_test.test_skids, remote_instance=self.rm), pd.DataFrame)
 
     def test_get_neuron(self):
         self.assertIsInstance(pymaid.get_neuron(
-            example_skids, remote_instance=self.rm), core.CatmaidNeuronList)
+            config_test.test_skids, remote_instance=self.rm), pymaid.CatmaidNeuronList)
 
     def test_get_neuron(self):
         self.assertIsInstance(pymaid.get_arbor(
-            example_skids[0], remote_instance=self.rm), pd.DataFrame)
+            config_test.test_skids[0], remote_instance=self.rm), pd.DataFrame)
 
     def test_get_treenode_table(self):
         self.assertIsInstance(pymaid.get_treenode_table(
-            example_skids, remote_instance=self.rm), pd.DataFrame)
+            config_test.test_skids, remote_instance=self.rm), pd.DataFrame)
 
     def test_get_review(self):
         self.assertIsInstance(pymaid.get_review(
-            example_skids, remote_instance=self.rm), pd.DataFrame)
+            config_test.test_skids, remote_instance=self.rm), pd.DataFrame)
 
     def test_get_volume(self):
         self.assertIsInstance(pymaid.get_volume(
-            example_volumes[0], remote_instance=self.rm), dict)
+            config_test.test_volume, remote_instance=self.rm), dict)
 
     def test_get_logs(self):
         self.assertIsInstance(pymaid.get_logs(
@@ -135,28 +156,27 @@ class TestPymaid(unittest.TestCase):
 
     def test_get_contributor_stats(self):
         self.assertIsInstance(pymaid.get_contributor_statistics(
-            example_skids, remote_instance=self.rm), pd.DataFrame)
+            config_test.test_skids, remote_instance=self.rm), pd.Series)
 
     def test_get_partners(self):
         self.assertIsInstance(pymaid.get_partners(
-            example_skids[0], remote_instance=self.rm), pd.DataFrame)
+            config_test.test_skids[0], remote_instance=self.rm), pd.DataFrame)
 
     def test_get_names(self):
-        names = pymaid.get_names(example_skids, remote_instance=self.rm)
+        names = pymaid.get_names(config_test.test_skids, remote_instance=self.rm)
         self.assertIsInstance(names, dict)
         self.assertIsInstance(pymaid.get_skids_by_name(
             list(names.values()), remote_instance=self.rm), pd.DataFrame)
 
     def test_get_edges(self):
-        self.assertIsInstance(pymaid.get_partners('annotation:%s' % example_annotations[
+        self.assertIsInstance(pymaid.get_partners('annotation:%s' % config_test.test_annotations[
                               0], remote_instance=self.rm), pd.DataFrame)
 
     def test_get_connectors(self):
-        cn = pymaid.get_connectors(example_skids, remote_instance=self.rm)
+        cn = pymaid.get_connectors(config_test.test_skids, remote_instance=self.rm)
         self.assertIsInstance(cn, pd.DataFrame)
         self.assertIsInstance(pymaid.get_connector_details(
-            cn.connector_id.tolist(), remote_instance=self.rm), pd.DataFrame)
-
+            cn.connector_id.tolist(), remote_instance=self.rm), pd.DataFrame)    
 
 class TestCore(unittest.TestCase):
     """Test pymaid.core """
@@ -169,67 +189,85 @@ class TestCore(unittest.TestCase):
             config_test.token,
             logger_level='ERROR')
 
-    def test_init(self):
-        self.assertIsInstance(core.CatmaidNeuron(
-            example_skids[0]), core.CatmaidNeuron)
-        self.assertIsInstance(core.CatmaidNeuronList(
-            example_skids), core.CatmaidNeuronList)
-
-    def test_copy(self):
-        nl = pymaid.get_neuron('annotation:%s' % example_annotations[
+        self.nl = pymaid.get_neuron('annotation:%s' % config_test.test_annotations[
                                0], remote_instance=self.rm)
-        self.assertIsInstance(nl[0].copy(), core.CatmaidNeuron)
-        self.assertIsInstance(nl.copy(), core.CatmaidNeuronList)
+
+    def test_init(self):
+        self.assertIsInstance(pymaid.CatmaidNeuron(
+            config_test.test_skids[0]), pymaid.CatmaidNeuron)
+        self.assertIsInstance(pymaid.CatmaidNeuronList(
+            config_test.test_skids), pymaid.CatmaidNeuronList)
+
+    def test_loaddata(self):
+        # Test explicit loading
+        nl = pymaid.CatmaidNeuron(config_test.test_skids[0])
+        nl.get_skeleton()
+        self.assertIsInstance(nl.nodes, pd.DataFrame)
+        # Test implicit loading
+        nl = pymaid.CatmaidNeuron(config_test.test_skids[0])        
+        self.assertIsInstance(nl.nodes, pd.DataFrame)
+
+    def test_copy(self):        
+        self.assertIsInstance(self.nl[0].copy(), pymaid.CatmaidNeuron)
+        self.assertIsInstance(self.nl.copy(), pymaid.CatmaidNeuronList)
 
     def test_summary(self):
-        nl = pymaid.get_neuron('annotation:%s' % example_annotations[
-                               0], remote_instance=self.rm)
-        self.assertIsInstance(nl[0].summary(), pd.Series)
-        self.assertIsInstance(nl.summary(), pd.DataFrame)
+        self.assertIsInstance(self.nl[0].summary(), pd.Series)
+        self.assertIsInstance(self.nl.summary(), pd.DataFrame)
 
-        self.assertIsInstance(nl.sum(), pd.Series)
-        self.assertIsInstance(nl.mean(), pd.Series)
+        self.assertIsInstance(self.nl.sum(), pd.Series)
+        self.assertIsInstance(self.nl.mean(), pd.Series)    
 
-    def test_downsampling(self):
-        nl = pymaid.get_neuron('annotation:%s' % example_annotations[
-                               0], remote_instance=self.rm)
-        nl2 = nl.copy()
-        nl2.downsample(4)
-        self.assertLess(nl2.n_nodes.sum(), nl.n_nodes.sum())
-
-    def test_prune_by_strahler(self):
-        nl = pymaid.get_neuron('annotation:%s' % example_annotations[
-                               0], remote_instance=self.rm)
-        nl2 = nl.copy()
-        nl2.prune_by_strahler()
-        self.assertLess(nl2.n_nodes.sum(), nl.n_nodes.sum())
-
-    def test_reload(self):
-        nl = pymaid.get_neuron('annotation:%s' % example_annotations[
-                               0], remote_instance=self.rm)
-        nl.reload()
-        self.assertIsInstance(nl, core.CatmaidNeuronList)
+    def test_reload(self):        
+        self.nl.reload()
+        self.assertIsInstance(self.nl, pymaid.CatmaidNeuronList)
 
     def test_graph_related(self):
-        nl = pymaid.get_neuron('annotation:%s' % example_annotations[
+        nl = pymaid.get_neuron('annotation:%s' % config_test.test_annotations[
                                0], remote_instance=self.rm)
-        self.assertIsInstance(nl[0].graph, nx.Graph)
-        self.assertIsInstance(nl[0].slabs, list)
+        self.assertIsInstance(self.nl[0].graph, nx.Graph)
+        self.assertIsInstance(self.nl[0].segments, list)
 
-    def test_indexing(self):
-        nl = pymaid.get_neuron('annotation:%s' % example_annotations[
-                               0], remote_instance=self.rm)
-        skids = nl.skeleton_id
+    def test_indexing(self):        
+        skids = self.nl.skeleton_id
 
-        self.assertIsInstance(nl[0], core.CatmaidNeuron)
-        self.assertIsInstance(nl[:3], core.CatmaidNeuronList)
-        self.assertIsInstance(nl[nl.n_nodes > 1000], core.CatmaidNeuronList)
-        self.assertIsInstance(nl[list(skids[:-5])], core.CatmaidNeuronList)
-        self.assertIsInstance(nl - nl[0], core.CatmaidNeuronList)
-        self.assertIsInstance(nl[0] + nl[1], core.CatmaidNeuronList)
-        self.assertIsInstance(nl + nl[1], core.CatmaidNeuronList)
+        self.assertIsInstance(self.nl[0], pymaid.CatmaidNeuron)
+        self.assertIsInstance(self.nl[:3], pymaid.CatmaidNeuronList)
+        self.assertIsInstance(self.nl[self.nl.n_nodes > 1000], pymaid.CatmaidNeuronList)
+        self.assertIsInstance(self.nl[list(skids[:-1])], pymaid.CatmaidNeuronList)
+        self.assertIsInstance(self.nl - self.nl[0], pymaid.CatmaidNeuronList)
+        self.assertIsInstance(self.nl[0] + self.nl[1], pymaid.CatmaidNeuronList)
+        self.assertIsInstance(self.nl + self.nl[1], pymaid.CatmaidNeuronList)
 
-        self.assertIsInstance(nl.sample(2), core.CatmaidNeuronList)
+        self.assertIsInstance(self.nl.sample(2), pymaid.CatmaidNeuronList)
+ 
+
+class TestMorpho(unittest.TestCase):
+    """ Test morphological operations """
+
+    def setUp(self):
+        self.rm = pymaid.CatmaidInstance(
+            config_test.server_url,
+            config_test.http_user,
+            config_test.http_pw,
+            config_test.token,
+            logger_level='ERROR')
+
+        self.nl = pymaid.get_neuron( config_test.test_skids, 
+                                     remote_instance=self.rm)
+
+    def test_downsampling(self):
+        nl2 = self.nl.copy()
+        nl2.downsample(4)
+        self.assertLess(nl2.n_nodes.sum(), self.nl.n_nodes.sum())
+
+    def test_resampling(self):
+        nl2 = self.nl.resample(10000, inplace=False)        
+        self.assertNotEqual(nl2.n_nodes.sum(), self.nl.n_nodes.sum())
+
+    def test_prune_by_strahler(self):        
+        nl2 = self.nl.prune_by_strahler(inplace=False)
+        self.assertLess(nl2.n_nodes.sum(), self.nl.n_nodes.sum())
 
 
 class TestPlot(unittest.TestCase):
@@ -243,31 +281,32 @@ class TestPlot(unittest.TestCase):
             config_test.token,
             logger_level='ERROR')
 
-    def test_plot3d_vispy(self):
-        nl = pymaid.get_neuron('annotation:%s' % example_annotations[
-                               0], remote_instance=self.rm)
+        self.nl = pymaid.get_neuron( config_test.test_skids, 
+                                     remote_instance=self.rm)
 
-        self.assertIsNotNone(nl.plot3d())
-        self.assertIsNotNone(nl.plot3d(volumes=example_volumes))
+        self.vol = pymaid.get_volume( config_test.test_volume )
+
+    def test_plot3d_vispy(self):
+        self.assertIsNotNone(self.nl.plot3d(backend='vispy'))
+        pymaid.close3d()
+        self.assertIsNotNone(pymaid.plot3d(self.nl, backend='vispy'))
+        pymaid.close3d()
+        self.assertIsNotNone(pymaid.plot3d([self.nl, self.vol], backend='vispy'))
+        pymaid.close3d()
 
     def test_plot3d_plotly(self):
-        nl = pymaid.get_neuron('annotation:%s' % example_annotations[
-                               0], remote_instance=self.rm)
-
-        self.assertIsNotNone(nl.plot3d(backend='plotly'))
-        self.assertIsNotNone(
-            nl.plot3d(backend='plotly', volumes=example_volumes))
+        self.assertIsNotNone(self.nl.plot3d(backend='plotly'))        
+        self.assertIsNotNone(pymaid.plot3d(self.nl, backend='plotly'))        
+        self.assertIsNotNone(pymaid.plot3d([self.nl, self.vol], backend='plotly'))
 
     def test_plot2d(self):
-        nl = pymaid.get_neuron('annotation:%s' % example_annotations[
-                               0], remote_instance=self.rm)
-
-        self.assertIsNotNone(nl.plot2d())
+        self.assertIsNotNone(self.nl.plot2d(method='2d'))
+        self.assertIsNotNone(self.nl.plot2d(method='3d_complex'))
+        self.assertIsNotNone(self.nl.plot2d(method='3d'))
 
     def tearDown(self):
-        plot.close3d()
+        pymaid.close3d()
         plt.clf()
-
 
 class TestUserStats(unittest.TestCase):
     """Test pymaid.user_stats """
@@ -280,32 +319,16 @@ class TestUserStats(unittest.TestCase):
             config_test.token,
             logger_level='ERROR')
 
+     
     def test_time_invested(self):
-        self.assertIsInstance(user_stats.get_time_invested(
-            example_skids, remote_instance=self.rm), pd.DataFrame)
-
+        self.assertIsInstance(pymaid.get_time_invested(
+            config_test.test_skids[0], remote_instance=self.rm), pd.DataFrame)
+    """
+    
     def test_user_contributions(self):
-        self.assertIsInstance(user_stats.get_user_contributions(
-            example_skids, remote_instance=self.rm), pd.DataFrame)
-
-
-class TestGraphCatmaid(unittest.TestCase):
-    """Test pymaid.igraph_catmaid """
-
-    def setUp(self):
-        self.rm = pymaid.CatmaidInstance(
-            config_test.server_url,
-            config_test.http_user,
-            config_test.http_pw,
-            config_test.token,
-            logger_level='ERROR')
-
-    def test_cluster_synapse_nodes(self):
-        nl = pymaid.get_neuron('annotation:%s' % example_annotations[
-                               0], remote_instance=self.rm)
-        self.assertIsInstance(graph.cluster_nodes_w_synapses(
-            nl[0]), np.ndarray)
-
+        self.assertIsInstance(pymaid.get_user_contributions(
+            config_test.test_skids, remote_instance=self.rm), pd.DataFrame)
+    """  
 
 if __name__ == '__main__':
     unittest.main()
