9354369df31a8a70ff67136f59294897d7c056b0
Philipp Schlegel <observing@web.de>
2018-05-15 17:19:17 +0100

    switch to setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except:
+    print('Setuptools not found - falling back to distutils')
+    from distutils.core import setup
 
 import re
 
@@ -27,12 +31,12 @@ setup(
 
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-        
+
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
 
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        
+
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
@@ -43,19 +47,17 @@ setup(
 
     install_requires=[
         "networkx>=2.1",
-        "scipy>=1.0.0",        
+        "scipy>=1.0.0",
         "numpy>=1.13.0",
-        "matplotlib>=2.1.0",
+        "matplotlib>=2.2.0",
         "plotly>=2.4.0",
         "pandas>=0.22.0",
-        "vispy>=0.5.0",
+        "vispy>=0.5.3",
         "tqdm>=4.14.0",
         "pyqt5",
         "pypng"
-        #"rpy2>=2.8.5", #This throws an error when no R is installed on the system 
+        #"rpy2>=2.8.5", #This throws an error when no R is installed on the system
     ],
 
     python_requires='>=3.4',
-
-    zip_safe = False
 )
