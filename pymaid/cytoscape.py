d52de01ea77dbc5648c4b16e86a67ab04ebc172e
Philipp Schlegel <schlegelp@users.noreply.github.com>
2018-06-04 09:35:27 -0400

    PEP8 compliance

--- pymaid/cytoscape.py
+++ pymaid/cytoscape.py
@@ -30,19 +30,21 @@ from py2cytoscape.data.cyrest_client import CyRestClient
 # Set up logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-if len( logger.handlers ) == 0:
+if len(logger.handlers) == 0:
     # Generate stream handler
     sh = logging.StreamHandler()
     sh.setLevel(logging.INFO)
     # Create formatter and add it to the handlers
     formatter = logging.Formatter(
-                '%(levelname)-5s : %(message)s (%(name)s)')
+        '%(levelname)-5s : %(message)s (%(name)s)')
     sh.setFormatter(formatter)
     logger.addHandler(sh)
+
 
 def get_client():
     """Initialises connection with Cytoscape and returns client."""
     return CyRestClient()
+
 
 def get_pymaid_style():
     """Returns our default network style."""
@@ -60,28 +62,29 @@ def get_pymaid_style():
     basic_settings = dict(
         # You can set default values as key-value pairs.
 
-        NODE_FILL_COLOR= '#FEC44F',
-        NODE_SIZE= 18,
-        NODE_BORDER_WIDTH= 7,
-        NODE_BORDER_COLOR= '#999999',
-        NODE_LABEL_COLOR= '#555555',
-        NODE_LABEL_FONT_SIZE= 14,
-        NODE_LABEL_POSITION= 'S,NW,c,0.00,3.00',
+        NODE_FILL_COLOR='#FEC44F',
+        NODE_SIZE=18,
+        NODE_BORDER_WIDTH=7,
+        NODE_BORDER_COLOR='#999999',
+        NODE_LABEL_COLOR='#555555',
+        NODE_LABEL_FONT_SIZE=14,
+        NODE_LABEL_POSITION='S,NW,c,0.00,3.00',
 
-        EDGE_WIDTH= 2,
-        EDGE_TRANSPARENCY= 100,
-        EDGE_CURVED= True,
-        EDGE_BEND= '0.728545744495502,-0.684997151948455,0.6456513365424503',
-        EDGE_UNSELECTED_PAINT= '#CCCCCC',
-        EDGE_STROKE_UNSELECTED_PAINT= '#333333',
-        EDGE_TARGET_ARROW_SHAPE= 'DELTA',
+        EDGE_WIDTH=2,
+        EDGE_TRANSPARENCY=100,
+        EDGE_CURVED=True,
+        EDGE_BEND='0.728545744495502,-0.684997151948455,0.6456513365424503',
+        EDGE_UNSELECTED_PAINT='#CCCCCC',
+        EDGE_STROKE_UNSELECTED_PAINT='#333333',
+        EDGE_TARGET_ARROW_SHAPE='DELTA',
 
-        NETWORK_BACKGROUND_PAINT= '#FFFFFF',
+        NETWORK_BACKGROUND_PAINT='#FFFFFF',
     )
 
     s.update_defaults(basic_settings)
 
     return s
+
 
 def generate_network(x, clear_session=True):
     """ Loads a CATMAID network into Cytoscape.
@@ -108,14 +111,15 @@ def generate_network(x, clear_session=True):
     if clear_session:
         cy.session.delete()
 
-    if isinstance(x, nx.Graph ):
+    if isinstance(x, nx.Graph):
         n = cy.network.create_from_networkx(x)
     elif isinstance(x, np.ndarray):
         n = cy.network.create_from_ndarray(x)
     elif isinstance(x, pd.DataFrame):
         n = cy.network.create_from_dataframe(x)
     else:
-        raise TypeError('Unable to generate network from data of type "{0}"'.format(type(x)))
+        raise TypeError(
+            'Unable to generate network from data of type "{0}"'.format(type(x)))
 
     # Apply basic layout
     cy.layout.apply(name='degree-circle', network=n)
@@ -124,17 +128,15 @@ def generate_network(x, clear_session=True):
     s = get_pymaid_style()
 
     # Add some passthough mappings to the style
-    s.create_passthrough_mapping(column='neuron_name', vp='NODE_LABEL', col_type='String')
+    s.create_passthrough_mapping(
+        column='neuron_name', vp='NODE_LABEL', col_type='String')
     max_edge_weight = n.get_edge_column('weight').max()
     s.create_continuous_mapping(column='weight', vp='EDGE_WIDTH', col_type='Double',
                                 points=[{'equal': '1.0', 'greater': '1.0', 'lesser': '1.0', 'value': 1.0},
-                                        {'equal': max_edge_weight/3, 'greater': 1.0, 'lesser': max_edge_weight/3, 'value': max_edge_weight}]
+                                        {'equal': max_edge_weight / 3, 'greater': 1.0, 'lesser': max_edge_weight / 3, 'value': max_edge_weight}]
                                 )
 
     # Apply style
     cy.style.apply(s, n)
 
     return n
-
-
-
