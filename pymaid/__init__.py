79d3c54ae28235684c221d7c0294ec8e592c0f7f
Philipp Schlegel <observing@web.de>
2021-01-06 09:56:41 +0000

    remove try/except from top level imports

--- pymaid/__init__.py
+++ pymaid/__init__.py
@@ -5,59 +5,15 @@ from . import config
 logger = config.logger
 
 # Flatten namespace by importing contents of all modules of pymaid
-try:
-    from .fetch import *
-except Exception as error:
-    logger.warning(str(error))
-    logger.warning('Error importing pymaid.fetch:\n' + str(error))
-
-try:
-    from .client import *
-except Exception as error:
-    logger.warning(str(error))
-    logger.warning('Error importing pymaid.client:\n' + str(error))
-
-try:
-    from .upload import *
-except Exception as error:
-    logger.warning(str(error))
-    logger.warning('Error importing pymaid.upload:\n' + str(error))
-
-try:
-    from .cluster import *
-except Exception as error:
-    logger.warning(str(error))
-    logger.warning('Error importing pymaid.cluster:\n' + str(error))
-
-try:
-    from .user_stats import *
-except Exception as error:
-    logger.warning(str(error))
-    logger.warning('Error importing pymaid.user_stats:\n' + str(error))
-
-try:
-    from .core import *
-except Exception as error:
-    logger.warning(str(error))
-    logger.warning('Error importing pymaid.core:\n' + str(error))
-
-try:
-    from .utils import *
-except Exception as error:
-    logger.warning(str(error))
-    logger.warning('Error importing pymaid.utils:\n' + str(error))
-
-try:
-    from .morpho import *
-except Exception as error:
-    logger.warning(str(error))
-    logger.warning('Error importing pymaid.morpho:\n' + str(error))
-
-try:
-    from .connectivity import *
-except Exception as error:
-    logger.warning(str(error))
-    logger.warning('Error importing pymaid.connectivity:\n' + str(error))
+from .fetch import *
+from .client import *
+from .upload import *
+from .cluster import *
+from .user_stats import *
+from .core import *
+from .utils import *
+from .morpho import *
+from .connectivity import *
 
 """
 try:
