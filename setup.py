0162df00a774e108b0816bafec9255da380c8f8e
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2023-11-14 10:30:04 +0000

    Drop python 3.8
    While it is still supported by PSF, it is deprecated by numpy's policy,
    which is a community standard among scientific packages.

--- setup.py
+++ setup.py
@@ -41,13 +41,12 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
 
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
     ],
     install_requires=requirements,
     extras_require={'extras': ['fuzzywuzzy[speedup]~=0.17.0',
                                'ujson~=1.35']},
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     zip_safe=False
 )
