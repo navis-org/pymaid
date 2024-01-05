d031f1c5bbb5538e4856dfccb46b440323a58fb1
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2024-01-05 16:59:10 +0000

    Add python 3.11

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     install_requires=requirements,
     extras_require={'extras': ['fuzzywuzzy[speedup]~=0.17.0',
